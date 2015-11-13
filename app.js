3ad8e65052475596595bf0181a08d0813cdfd966
=Ryan Brown <=ryanb@exitcare.com>
2015-11-13 16:21:22 -0600

    Basic Development

--- app.js
+++ app.js
@@ -4,6 +4,28 @@ var config = require('./config.json');
 
 start();
 
+function getDetails(fileUri) {
+  var jsonReply = "";
+  if(fileUri) {
+    fs.stat(fileUri, function(err, stats) {
+      if (err) {
+        console.log('\n' + err + '\n');
+        return;
+      }
+      if (stats.isDirectory()) {
+        return "Directory";
+      }
+      if (stats.isFile()) {
+        return "Is File ";
+      }
+    });
+  } else {
+    return "File Type Not Found";
+  }
+  //return jsonReply;// {"fileType":"Not Found"};
+
+};
+
 function start() {
   for(var path in config.configs.inputPaths) {
     var topDir = config.configs.inputPaths[path].path;
@@ -11,7 +33,12 @@ function start() {
     fs.readdir(topDir, function(err, files) {
 
       for(var item in files) {
-        console.log(files[item]);
+        var fullFileLocation = topDir + "\\" + files[item];
+        var fdetails = getDetails(fullFileLocation);
+
+        console.log(fdetails + " - " + fullFileLocation);
+
+
       }
     });
   // fs.stat(topDir, function(err, stats) {
@@ -20,6 +47,7 @@ function start() {
   //   }
   // })
   }
+
   //function processFileList()
   // function addToList(fileObj) {
   //
